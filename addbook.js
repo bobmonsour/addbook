480213520ec9c80110f7d7dacacd351b08baa9c1
Bob Monsour <bob.monsour@gmail.com>
2025-02-07 17:35:53 -0800

    First working version

--- addbook.js
+++ addbook.js
@@ -1,28 +1,7 @@
 #!/usr/bin/env node
 
-// A node script that I use to create a file for post, note, or til entry.
-// It prompts the user to enter a title, from which a filename is generated.
-// It then prompts for a few 'always present' front matter elements,
-// followed by some optional elements.
-//
-// Example front matter resulting from creating a file named a-test-post.md
-//
-//  ---
-//  title: A test post
-//  description: This is a test post being created as an example of using this tool.
-//  date: 2025-02-07
-//  tags: [
-//    "blogging",
-//    "personal"
-//  ]
-//  image:
-//    source: a-test-post.jpg
-//    alt: A blog post being displayed on a laptop
-//  ---
-
 import fs from "fs";
 import inquirer from "inquirer";
-import slugify from "slugify";
 import { format } from "date-fns";
 
 const today = format(new Date(), "yyyy-MM-dd");
@@ -36,101 +15,80 @@ async function promptUser() {
 		},
 		{
 			type: "input",
-			name: "date",
-			message: "Date (yyyy-mm-dd):",
+			name: "author",
+			message: "Author:",
+		},
+		{
+			type: "input",
+			name: "isbn",
+			message: "ISBN (13 characters):",
+			validate: (input) => {
+				const isValid = /^\d{13}$/.test(input);
+				return isValid || "ISBN must be a 13-character string of numbers.";
+			},
+		},
+		{
+			type: "input",
+			name: "rating",
+			message: "Rating (1-5 or leave blank):",
+			validate: (input) => {
+				if (input === "") return true;
+				const rating = Number(input);
+				return (
+					(rating >= 1 && rating <= 5) ||
+					"Rating must be a number between 1 and 5."
+				);
+			},
+		},
+		{
+			type: "input",
+			name: "yearRead",
+			message: "Year Read (yyyy-mm-dd):",
 			default: today,
-		},
-		{
-			type: "input",
-			name: "tags",
-			message: "Tags (comma-separated):",
-		},
-		{
-			type: "input",
-			name: "description",
-			message: "Description:",
+			validate: (input) => {
+				const isValid = /^\d{4}-\d{2}-\d{2}$/.test(input);
+				return isValid || "Date must be in the format yyyy-mm-dd.";
+			},
 		},
 		{
 			type: "confirm",
-			name: "pageHasCode",
-			message: "Page has code?",
+			name: "localCover",
+			message: "Local cover image? (y/n):",
 			default: false,
-		},
-		{
-			type: "confirm",
-			name: "snow",
-			message: "Snow?",
-			default: false,
-		},
-		{
-			type: "confirm",
-			name: "pageHasYoutube",
-			message: "Page has video?",
-			default: false,
-		},
-		{
-			type: "input",
-			name: "imageSource",
-			message: "Image source (leave blank to skip):",
-			default: (answers) => `${slugify(answers.title, { lower: true })}.jpg`,
-		},
-		{
-			type: "input",
-			name: "imageAlt",
-			message: "Image alt text:",
-			when: (answers) => answers.imageSource.trim() !== "",
 		},
 	]);
 
 	return answers;
 }
 
-function generateYamlContent(answers) {
-	const slugifiedTitle = slugify(answers.title, { lower: true });
-	const tagsArray = answers.tags
-		? answers.tags.split(",").map((tag) => tag.trim())
-		: [];
+function generateJsonContent(answers) {
+	const jsonContent = {
+		title: answers.title,
+		author: answers.author,
+		isbn: answers.isbn,
+		rating: answers.rating || "",
+		yearRead: answers.yearRead,
+	};
 
-	let yamlContent = `---
-title: ${answers.title}
-description: ${answers.description}
-date: ${answers.date}
-tags: ${JSON.stringify(tagsArray, null, 2)}
-`;
-
-	if (answers.pageHasCode) {
-		yamlContent += `pageHasCode: ${answers.pageHasCode}\n`;
-	}
-	if (answers.pageHasYoutube) {
-		yamlContent += `pageHapageHasYoutubesVideo: ${answers.pageHasYoutube}\n`;
-	}
-	if (answers.snow) {
-		yamlContent += `snow: ${answers.snow}\n`;
-	}
-	if (answers.imageSource.trim() !== "") {
-		yamlContent += `image:
-  source: ${answers.imageSource}
-  alt: ${answers.imageAlt || ""}
-`;
+	if (answers.localCover) {
+		jsonContent.localCover = true;
 	}
 
-	yamlContent += `---`;
-
-	return { yamlContent, slugifiedTitle };
+	return jsonContent;
 }
 
-async function confirmOrEditYaml(yamlContent, answers) {
+async function confirmOrEditJson(jsonContent, answers) {
 	let accept = false;
 
 	while (!accept) {
-		console.log("Generated YAML content:");
-		console.log(yamlContent);
+		console.log("Generated JSON content:");
+		console.log(JSON.stringify(jsonContent, null, 2));
 
 		const response = await inquirer.prompt([
 			{
 				type: "confirm",
 				name: "accept",
-				message: "Do you accept the generated YAML content?",
+				message: "Do you accept the generated JSON content?",
 				default: true,
 			},
 		]);
@@ -138,66 +96,8 @@ async function confirmOrEditYaml(yamlContent, answers) {
 		accept = response.accept;
 
 		if (!accept) {
-			answers = await inquirer.prompt([
-				{
-					type: "input",
-					name: "title",
-					message: "Title:",
-					default: answers.title,
-				},
-				{
-					type: "input",
-					name: "date",
-					message: "Date (yyyy-mm-dd):",
-					default: answers.date,
-				},
-				{
-					type: "input",
-					name: "tags",
-					message: "Tags (comma-separated):",
-					default: answers.tags,
-				},
-				{
-					type: "input",
-					name: "description",
-					message: "Description:",
-					default: answers.description,
-				},
-				{
-					type: "confirm",
-					name: "pageHasCode",
-					message: "Page has code?",
-					default: answers.pageHasCode,
-				},
-				{
-					type: "confirm",
-					name: "snow",
-					message: "Snow?",
-					default: answers.snow,
-				},
-				{
-					type: "confirm",
-					name: "pageHasYoutube",
-					message: "Page has video?",
-					default: answers.pageHasYoutube,
-				},
-				{
-					type: "input",
-					name: "imageSource",
-					message: "Image source (leave blank to skip):",
-					default: answers.imageSource,
-				},
-				{
-					type: "input",
-					name: "imageAlt",
-					message: "Image alt text:",
-					default: answers.imageAlt,
-					when: (answers) => answers.imageSource.trim() !== "",
-				},
-			]);
-
-			const result = generateYamlContent(answers);
-			yamlContent = result.yamlContent;
+			answers = await promptUser();
+			jsonContent = generateJsonContent(answers);
 		}
 	}
 
@@ -206,13 +106,22 @@ async function confirmOrEditYaml(yamlContent, answers) {
 
 async function main() {
 	let answers = await promptUser();
-	let { yamlContent, slugifiedTitle } = generateYamlContent(answers);
+	let jsonContent = generateJsonContent(answers);
 
-	answers = await confirmOrEditYaml(yamlContent, answers);
-	({ yamlContent, slugifiedTitle } = generateYamlContent(answers));
+	answers = await confirmOrEditJson(jsonContent, answers);
+	jsonContent = generateJsonContent(answers);
 
-	fs.writeFileSync(`${slugifiedTitle}.md`, yamlContent, "utf8");
-	console.log(`File ${slugifiedTitle}.md created successfully!`);
+	const filePath = "./books.json";
+	let booksArray = [];
+
+	if (fs.existsSync(filePath)) {
+		const fileContent = fs.readFileSync(filePath, "utf8");
+		booksArray = JSON.parse(fileContent);
+	}
+
+	booksArray.push(jsonContent);
+	fs.writeFileSync(filePath, JSON.stringify(booksArray, null, 2), "utf8");
+	console.log(`Book added to ${filePath} successfully!`);
 }
 
 main();
